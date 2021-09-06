41c753761f764d99c90f6066d773128bcd8403a9
cine6m <cine99m@gmail.com>
2021-09-07 00:59:14 +0900

    12

--- inventory.cpp
+++ inventory.cpp
@@ -543,23 +543,8 @@ void inventory::MenuCraftOpen()
         }
     }
 
-
-   
- 
-    
-   
-
-
 }
-//스탯창
-void inventory::MenuStatOpen()
-{
 
-}
-//제작창
-void inventory::MenuCraftOpen()
-{
-}
 //세팅창
 void inventory::MenuSettingOpen()
 {
@@ -609,21 +594,11 @@ void inventory::AddItem(item* item)
 }
 
 //메뉴버튼용
-
-    if (!_isadd)
-    {
-        _vInven[arrNum] = item;
-    }
-   
-}
-
-//버튼용
-
 void inventory::SelectMenu()
 {
-    if(PtInRect(&_storageRc,_ptMouse)||PtInRect(&_statRc,_ptMouse)
-       || PtInRect(&_craftRc, _ptMouse)|| PtInRect(&_settingRc, _ptMouse)|| PtInRect(&_exitRc,_ptMouse))
-    { 
+    if (PtInRect(&_storageRc, _ptMouse) || PtInRect(&_statRc, _ptMouse)
+        || PtInRect(&_craftRc, _ptMouse) || PtInRect(&_settingRc, _ptMouse) || PtInRect(&_exitRc, _ptMouse))
+    {
         if (KEYMANAGER->isOnceKeyDown(VK_LBUTTON))
         {
             if (PtInRect(&_storageRc, _ptMouse) && _menuPage != InvenPage)
@@ -631,7 +606,7 @@ void inventory::SelectMenu()
                 SOUNDMANAGER->play("menuSelect", 0.5f);
                 _currentMenuImg = _invenImg;
                 _menuPage = InvenPage;
-            
+
             }
             if (PtInRect(&_statRc, _ptMouse) && _menuPage != StatPage)
             {
@@ -654,12 +629,13 @@ void inventory::SelectMenu()
             if (PtInRect(&_exitRc, _ptMouse))
             {
                 SOUNDMANAGER->play("menuSelect", 0.5f);
-                _currentMenuImg = _exitImg;            
+                _currentMenuImg = _exitImg;
                 _menuPage = ExitPage;
             }
         }
     }
 }
+
 //나가기버튼 콜백함수
 void inventory::Button(void* obj)
 {
